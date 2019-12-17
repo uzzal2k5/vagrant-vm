a672c23afc03e1dafa5e3751854e5fa014947df1
Md Shafiqul Islam <uzzal@Mds-MacBook-Pro.local>
2019-12-18 03:40:10 +0600

    Create PersistentVolume  cluster during provision

--- kubernetes-cluster/kubernetes/deployment.sh
+++ kubernetes-cluster/kubernetes/deployment.sh
@@ -3,4 +3,5 @@ set -ex
 K8S_MASTER_IP="192.168.10.10"
 sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no -t vagrant@"$K8S_MASTER_IP" sudo --user=vagrant  bash dashboard.sh
 sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no -t vagrant@"$K8S_MASTER_IP" sudo --user=vagrant  bash helm-bootstrap.sh
+sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no -t vagrant@"$K8S_MASTER_IP" sudo --user=vagrant  kubectl apply -f PersistentVolume.yaml
 #sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no -t vagrant@"$K8S_MASTER_IP" sudo --user=vagrant  bash deploy-gitlab.sh
